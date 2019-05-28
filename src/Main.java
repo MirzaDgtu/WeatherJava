3192dcb9a287c2c75ba55a7fdf30ae74d85a41ce
Mirzali Pirmagomedov <mirzyali@mail.ru>
2019-05-29 00:32:41 +0700

    change main.java

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,9 @@ public class Main {
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
         CelsiyDisplay  celsiyDisplay = new CelsiyDisplay(weatherData);
-        // ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
 
         weatherData.setMeasurements(80,65,30.4f);
         weatherData.setMeasurements(82,70,28.2f);
-        weatherData.setMeasurements(78,90,29.3f);
-        weatherData.setMeasurements(100, 90,29.9f);
     }
 }
